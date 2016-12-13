646b4b3586aa926ebac8108cb79a8b1f1224d176
ito <ito@ito>
2016-12-13 09:44:30 +0900

    apply / candidate 70% _hshs

--- TeamSNS/src/main/java/edu/hexa/leejaehoon/persistence/ProjectInfoDAO.java
+++ TeamSNS/src/main/java/edu/hexa/leejaehoon/persistence/ProjectInfoDAO.java
@@ -2,6 +2,8 @@ package edu.hexa.leejaehoon.persistence;
 
 import java.util.List;
 
+import edu.hexa.leejaehoon.domain.MyApplyDTO;
+import edu.hexa.leejaehoon.domain.MyCandidateDTO;
 import edu.hexa.leejaehoon.domain.ProjectInfoVO;
 import edu.hexa.teamsns.domain.ProjectVO;
 import edu.hexa.teamsns.domain.RecruitProjectVO;
@@ -17,4 +19,11 @@ public interface ProjectInfoDAO {
 	public abstract int update2(ProjectVO vo);
 	
 	public abstract int update3(RequiredSkillVO vo);
+	
+	
+	// hshs
+	public abstract List<MyApplyDTO> select_myapply(String uid); // 지원한 프로젝트 관리
+	public abstract List<MyCandidateDTO> select_candidate(String uid); // 지원자 관리
+	public abstract int applyReject(String part_pk, String user_id);
+	
 } // end interface
